b2e1a7c0d123aad1d86d5419d79864b33c63c03b
Joshua Ragasa <jpragasa@seu.edu>
2020-11-18 11:57:42 -0500

    Finished Quaternions

---
+++ UnitySurvivalGuide/Assets/Rotations/QuatSlerp/AimSlerp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimSlerp : MonoBehaviour
+{
+    [SerializeField] private Transform _sphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Formula for direction
+        // Direction = destination - source
+        Vector3 directionToFace = _sphere.position - transform.position;
+        Debug.DrawRay(transform.position, directionToFace, Color.cyan);
+        // Access current rotation = Quaternion.lookrotation
+        Quaternion targetRotation = Quaternion.LookRotation(directionToFace);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+    }
+}
---
+++ UnitySurvivalGuide/Assets/Rotations/QuaternionIdentity/CubeRotation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+    [SerializeField] private GameObject cube;
+    [SerializeField] private float offset;
+    [SerializeField] private int reps;
+    private bool flag = false;
+    private int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation();
+    }
+
+    public void rotation()
+    {
+        counter++;
+        if(counter == reps)
+        {
+            flag = !flag;
+            offset = 0;
+            counter = 0;
+        }
+        if(!flag)
+        {
+            cube.transform.rotation = Quaternion.Euler(cube.transform.rotation.x + offset, cube.transform.rotation.y - offset, 0);
+            offset--;
+        }
+        else
+        {
+            cube.transform.rotation = Quaternion.Euler(cube.transform.rotation.x - offset, cube.transform.rotation.y + offset, 0);
+            offset--;
+        }
+    }
+}
