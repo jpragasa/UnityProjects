1f189b4c8de5ebfde06d67c3d9efd6f5f3d84739
Joshua Ragasa <jpragasa@seu.edu>
2020-11-29 19:27:55 -0500

    Bank system

---
+++ UnitySurvivalGuide/Assets/Classes/BankSys/Bank.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Bank
+{
+    public string branchName;
+    public string location;
+    public float cashInVault;
+
+    public void CheckBalance()
+    {
+        Debug.Log("Checking Balance at " + this.branchName);
+    }
+
+    public void Withdraw()
+    {
+        Debug.Log("Withdrawing from " + this.branchName);
+    }
+
+    public void Deposit()
+    {
+        Debug.Log("Depositing to " + this.branchName);
+    }
+}
