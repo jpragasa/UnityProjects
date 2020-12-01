e8d6a17f3fa2c0fd80088caddb01712ca2288784
Joshua Ragasa <jpragasa@seu.edu>
2020-12-01 12:40:39 -0500

    Finished Properties

---
+++ UnitySurvivalGuide/Assets/Properties/Challenge/PropertyChallenge.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PropertyChallenge : MonoBehaviour
+{
+    private int speed;
+    //private string name;
+
+    public int Speed
+    {
+        get
+        {
+            return this.speed;
+        }
+
+        private set
+        {
+            this.speed = value;
+        }
+    }
+
+    public string Name { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Name = "Josh";
+        speed = 200;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(string.Format("Name: {0} Speed: {1}", Name, speed));
+            speed -= 1;
+        }
+    }
+}
---
+++ UnitySurvivalGuide/Assets/StaticTypes/StaticConstr/StaticMembers.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Employee
+{
+    public int employeeID;
+    public string first, last;
+    public int salary;
+
+    public static string company;
+
+    public Employee()
+    {
+        Debug.Log("Instance Members Initialized");
+    }
+
+    static Employee()
+    {
+        company = "Symphonic Games";
+        Debug.Log("Static Members Initialized");
+    }
+}
+public class StaticMembers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Employee e1 = new Employee();
+        var e2 = new Employee();
+        var e3 = new Employee();
+        var e4 = new Employee();
+        Employee e5 = new Employee();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
