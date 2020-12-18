53a60625c8b01ea96da4887a35812f48d6e0403c
Joshua Ragasa <jpragasa@seu.edu>
2020-12-17 21:40:48 -0500

    Unit One

---
+++ Unit One Player Control/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform objTransform;
+    [SerializeField] private float cameraOffsetY;
+    [SerializeField] private float cameraOffsetX;
+    [SerializeField] private float cameraOffsetZ;
+    private Vector3 fullOffSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fullOffSet = new Vector3(cameraOffsetX, cameraOffsetY, cameraOffsetZ);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        transform.position = objTransform.transform.position + fullOffSet;
+    }
+}
---
+++ Unit One Player Control/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private float speedOffset;
+
+    public enum Directions
+    {
+        FORWARD,
+        LEFT,
+        BACK,
+        RIGHT
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Controls();
+    }
+
+    private void Controls()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            Move(Directions.FORWARD);
+        } 
+        if(Input.GetKey(KeyCode.A))
+        {
+            Move(Directions.LEFT);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Move(Directions.RIGHT);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Move(Directions.BACK);
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            speedOffset += .5f;
+        }
+    }
+
+    private void Move(Directions directions)
+    {
+        switch(directions)
+        {
+            case Directions.FORWARD:
+                transform.Translate(Vector3.forward * speedOffset * Time.deltaTime);
+                break;
+            case Directions.LEFT:
+                transform.Rotate(Vector3.down * speedOffset * Time.deltaTime);
+                break;
+            case Directions.RIGHT:
+                transform.Rotate(Vector3.up * speedOffset * Time.deltaTime);
+                break;
+            case Directions.BACK:
+                transform.Translate(Vector3.back * speedOffset * Time.deltaTime);
+                break;
+        }
+    }
+}
