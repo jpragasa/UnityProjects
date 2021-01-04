1b12b651e69bc6dae79b595413bd3b42687a97ef
Joshua Ragasa <jpragasa@seu.edu>
2021-01-03 20:38:59 -0500

    Clicky Game Init

---
+++ Clicky Mouse/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private static SpawnManager _instance;
+    [SerializeField] private List<GameObject> _enemyList;
+    [SerializeField] private List<GameObject> _enemies;
+    [SerializeField] private List<Transform> _parentTransform;
+    ///[SerializeField] private List<Transform> _parentTransform;
+    [SerializeField] private GameObject _enemyContainer;
+    [SerializeField] private float _spawnDelayMin = 3f;
+    [SerializeField] private float _spawnDelayMax = 6f;
+
+    private static GameObject previous;
+    private static GameObject _oneBeforePrevious;
+    public static SpawnManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.Log("Spawn Manager does not exist");
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    private void Start()
+    {
+        _oneBeforePrevious = null;
+        GenerateEnemies(_enemies.Count);
+        StartCoroutine(Spawn());
+    }
+
+    public GameObject RequestEnemy()
+    {
+        foreach(var enemy in _enemyList)
+        {
+            if(enemy.activeInHierarchy == false && enemy != previous && enemy != _oneBeforePrevious)
+            {
+                enemy.SetActive(true);
+                _oneBeforePrevious = previous;
+                if(_oneBeforePrevious == null)
+                {
+                    previous = enemy;
+                    return enemy;
+                }
+                else
+                {
+                    previous = enemy;
+                    return enemy;
+                }                
+            }
+        }
+
+        float randomizer = Random.Range(0, _enemies.Count);
+        GameObject newEnemy = Instantiate(_enemies[(int)randomizer]);
+        newEnemy.transform.parent = _enemyContainer.transform;
+        newEnemy.SetActive(true);
+        _enemyList.Add(newEnemy);
+        return newEnemy;
+    }
+
+    public void DeactivateEnemies()
+    {
+        foreach(var obj in _enemyList)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    private List<GameObject> GenerateEnemies(int amountOfEnemies)
+    {
+        for(int i = 0; i < amountOfEnemies; i++)
+        {
+            //float randomizer = Random.Range(0, _enemies.Count);
+            GameObject obj = Instantiate(this._enemies[i]);
+            _parentTransform[i] = _enemyContainer.transform;
+            _enemies[i].SetActive(false);
+            _enemyList.Add(obj);
+        }
+
+        return _enemyList;
+    }
+
+    IEnumerator Spawn()
+    {
+        while(true)
+        {
+            GameObject obj = Instance.RequestEnemy();
+            obj.transform.position = new Vector3(Random.Range(-4, 4), -4);
+            yield return new WaitForSeconds(Random.Range(_spawnDelayMin, _spawnDelayMax));
+        }
+    }
+}
---
+++ Clicky Mouse/Assets/Scripts/Target.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    [SerializeField] private float minSpeed = 12;
+    [SerializeField] private float maxSpeed = 16;
+    [SerializeField] private float maxTorque = 10;
+    [SerializeField] private float xRange = 4;
+    [SerializeField] private float ySpawnPos = -6;
+    [SerializeField] private float despawnDelayX = 5f;
+    [SerializeField] private float despawnDelayY = 10f;
+    private Vector3 originalVelocity;
+    private Vector3 originalAngularVelocity;
+
+    private void Awake()
+    {
+        originalVelocity = GetComponent<Rigidbody>().velocity;
+        originalAngularVelocity = GetComponent<Rigidbody>().angularVelocity;
+    }
+
+    private void OnEnable()
+    {
+        this.gameObject.GetComponent<Rigidbody>().velocity = originalVelocity;
+        this.gameObject.GetComponent<Rigidbody>().angularVelocity = originalAngularVelocity;
+        // Get RigidBody
+        targetRb = GetComponent<Rigidbody>();
+        // Target is thrown upwards   
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        // Target rotates and random values
+        targetRb.AddTorque(RandomTorque(),
+                           RandomTorque(),
+                           RandomTorque(), ForceMode.Impulse);
+        // Target spawnes at a Random Location        
+        transform.position = RandomSpawn();   
+        Invoke("Hide", Random.Range(despawnDelayX, despawnDelayY));
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomSpawn()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    
+    private void Hide()
+    {        
+        this.gameObject.SetActive(false);
+    }
+    
+
+    private void OnMouseDown()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        this.gameObject.SetActive(false);
+    }
+    */
+}
