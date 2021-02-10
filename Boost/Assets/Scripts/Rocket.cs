885de23f0421b58053a3f25c0063f1d4d09d9397
jpragasa <jpragasa12@gmail.com>
2021-02-09 21:51:49 -0500

    Movement update and audio

--- Boost/Assets/Scripts/Rocket.cs
+++ Boost/Assets/Scripts/Rocket.cs
@@ -7,33 +7,51 @@ public class Rocket : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody rb;
+    private AudioSource audioSource;
     [SerializeField] private float _thrustSpeed;
     [SerializeField] private float _rotationSpeed;
+
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody>();
+        audioSource = this.gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        ProcessInput();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Rotate()
     {
-        if(Input.GetKey(KeyCode.Space))
+        Thrust();
+        rb.freezeRotation = true; // take manual control of rotation
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.gameObject.transform.Rotate(Vector3.forward * _rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.gameObject.transform.Rotate(Vector3.back * _rotationSpeed * Time.deltaTime);
+        }
+        rb.freezeRotation = false;
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
         {
             // AddRelativeForce allows the object to go in the direction it is facing
-            rb.AddRelativeForce(Vector3.up * _thrustSpeed * Time.deltaTime, ForceMode.Impulse);
+            rb.AddRelativeForce(Vector3.up * _thrustSpeed * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(audioSource.clip);
+            }
         }
-        if(Input.GetKey(KeyCode.A))
+        else
         {
-            this.gameObject.transform.Rotate(Vector3.forward * _rotationSpeed);
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            this.gameObject.transform.Rotate(Vector3.back * _rotationSpeed);
+            audioSource.Stop();
         }
     }
 }
