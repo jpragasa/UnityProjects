9239e7dbd6654bf6b7169a4c90c2dff5a2bee344
jpragasa <jpragasa12@gmail.com>
2021-02-21 18:53:10 -0500

    Boost Done

---
+++ Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 _movementVector = new Vector3(10f, 10f, 10f);
+    //Period: the time it takes to complete one full cycle
+    [SerializeField] private float period = 2f;
+    private Vector3 _startingPos;
+    [Range(0, 1)] [SerializeField] private float movementFactor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        // Protects from period equalling zero
+        if(period <= Mathf.Epsilon) { return; }
+
+        float cycles = Time.time / period; // Grows continually from 0
+
+        // Defining Tau
+        const float tau = Mathf.PI * 2; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to 1
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+
+        Vector3 offset = _movementVector * movementFactor;
+        transform.position = _startingPos + offset;    
+    }
+}
--- Boost/Assets/Scripts/Rocket.cs
+++ Boost/Assets/Scripts/Rocket.cs
@@ -13,6 +13,13 @@ public class Rocket : MonoBehaviour
     [SerializeField] private float _rotationSpeed;
     [SerializeField] private float _respawnTime;
     [SerializeField] private float _loadWaitTime;
+    [SerializeField] private bool _debugMode = false;
+    [SerializeField] private AudioClip[] _audioClips;
+    [SerializeField] private ParticleSystem _thrustParticles;
+    [SerializeField] private ParticleSystem _successParticles;
+    [SerializeField] private ParticleSystem _deathParticles;
+    
+    
 
     enum State { Alive, Dying, Transcending};
     State state = State.Alive;
@@ -21,17 +28,36 @@ public class Rocket : MonoBehaviour
     {
         rb = this.gameObject.GetComponent<Rigidbody>();
         audioSource = this.gameObject.GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(state == State.Alive)
+        // Ensures that this does not get implemented in a final build
+        if(Debug.isDebugBuild)
+        {
+            Debuggers();
+        }
+        
+        if (state == State.Alive)
         {
             Thrust();
             Rotate();
         }
-        
+
+    }
+
+    private void Debuggers()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            _debugMode = !_debugMode;
+        }
     }
 
     private void Rotate()
@@ -53,22 +79,29 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            // AddRelativeForce allows the object to go in the direction it is facing
-            rb.AddRelativeForce(Vector3.up * _thrustSpeed * Time.deltaTime);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(audioSource.clip);
-            }
+            ApplyThrust();
         }
         else
         {
             audioSource.Stop();
+            _thrustParticles.Stop();
         }
+    }
+
+    private void ApplyThrust()
+    {
+        // AddRelativeForce allows the object to go in the direction it is facing
+        rb.AddRelativeForce(Vector3.up * _thrustSpeed * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            PlayAudio(0);
+        }
+        _thrustParticles.Play();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(state != State.Alive)
+        if(state != State.Alive || _debugMode == true)
         {
             return;
         }
@@ -80,32 +113,58 @@ public class Rocket : MonoBehaviour
                 break;
             case "Finish":
                 state = State.Transcending;
+                audioSource.Stop();
+                _successParticles.Play();
+                PlayAudio(1);              
                 Invoke("LoadNextLevel", _loadWaitTime);
                 break;
             default:
                 // Death
                 state = State.Dying;
+                _deathParticles.Play();
                 audioSource.Stop();
                 StartCoroutine(FreezePosition());
                 break;
         }
     }
 
+    private void PlayAudio(int clipIndex)
+    {
+        if(!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(_audioClips[clipIndex]);
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
     private void LoadNextLevel()
     {
-        SceneManager.LoadScene(1);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex + 1 > SceneManager.sceneCount)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
         state = State.Alive;
     }
 
-    private void LoadFirstLevel()
+    private void LoadCurrentScene()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     IEnumerator FreezePosition()
     {
+        audioSource.Stop();
+        PlayAudio(2);
         yield return new WaitForSeconds(_respawnTime);
-        Invoke("LoadFirstLevel", _loadWaitTime);
+        Invoke("LoadCurrentScene", _loadWaitTime);
         state = State.Alive;
     }
 }
