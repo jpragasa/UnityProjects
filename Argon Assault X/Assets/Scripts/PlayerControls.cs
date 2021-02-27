7e4b42b22519f238c6be638dd14a5ed0869445f9
jpragasa <jpragasa12@gmail.com>
2021-02-27 10:53:27 -0500

    Reverted Back To Old Input System

---
+++ Argon Assault X/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = Input.GetAxis("Horizontal");
+        float verticalThrow = Input.GetAxis("Vertical");
+        transform.Rotate(0,0,-horizontalThrow);
+        transform.Rotate(verticalThrow,0,0);
+    }
+}
