2186db2b09439bbdd702283260872e7e3ef1694e
jpragasa <jpragasa12@gmail.com>
2021-02-27 23:29:01 -0500

    Added Lasers

--- Argon Assault X/Assets/Scripts/PlayerControls.cs
+++ Argon Assault X/Assets/Scripts/PlayerControls.cs
@@ -5,7 +5,15 @@ using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] InputAction movement;
-    [SerializeField] float offset;
+    [SerializeField] float controlSpeed;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 5f;
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float controlRollFactor = -20f;
+    float xThrow, yThrow;
+    
     private void OnEnable()
     {
         movement.Enable();
@@ -16,22 +24,39 @@ public class PlayerControls : MonoBehaviour
         movement.Disable();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         Movement();
+        Rotation();
     }
 
     private void Movement()
     {
-        float xThrow = movement.ReadValue<Vector2>().x;
-        float yThrow = movement.ReadValue<Vector2>().y;
+        xThrow = movement.ReadValue<Vector2>().x;
+        yThrow = movement.ReadValue<Vector2>().y;
 
-        transform.localPosition = new Vector3(xThrow * offset * Time.deltaTime,
-                                              transform.localPosition.y,
-                                              transform.localPosition.z);
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
-        transform.localPosition = new Vector3(transform.localPosition.x,
-                                              yThrow * offset * Time.deltaTime,
-                                              transform.localPosition.z);
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+
+    }
+
+    private void Rotation()
+    {
+        // Y Rotation
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+        // X Rotation
+        float yaw = transform.localPosition.x * positionYawFactor;
+        // Z Rotation
+        float roll = xThrow * controlRollFactor;
+
+        // Controls local location of ship, implements Euler
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
