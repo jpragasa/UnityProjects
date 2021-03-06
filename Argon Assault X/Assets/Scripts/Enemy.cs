ea5382a3b1173db62a88883e218577993a4932b4
jpragasa <jpragasa12@gmail.com>
2021-03-06 15:08:26 -0500

    More Timelines Added

--- Argon Assault X/Assets/Scripts/Enemy.cs
+++ Argon Assault X/Assets/Scripts/Enemy.cs
@@ -1,36 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
 
     [SerializeField] GameObject deathVFX;
-    [SerializeField] Transform parent;
+    [SerializeField] GameObject hitVFX;    
     [SerializeField] int pointsToAdd;
+    [SerializeField] int hitPoints;
 
     ScoreBoard scoreBoard;
-
+    GameObject parentGameObject;
     private void Start()
     {
+        SetUp();
+    }
+
+    private void SetUp()
+    {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        AddRigidBody();
+    }
+
+    private void AddRigidBody()
+    {
+        Rigidbody rb = this.gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     private void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        DestroyEnemy();
-    }
+        HealthDetection();
+    }   
+
     private void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;  
         scoreBoard.IncreaseScore(pointsToAdd);
+        hitPoints--;
     }
     private void DestroyEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(this.gameObject);
     }
 
-    
+    private void HealthDetection()
+    {
+        if (hitPoints < 1)
+        {
+            DestroyEnemy();
+        }
+    }
 }
