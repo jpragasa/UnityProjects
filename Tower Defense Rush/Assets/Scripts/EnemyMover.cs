e46cfd618e285eac471a243d7dcd23f933f3eece
jpragasa <jpragasa12@gmail.com>
2021-03-07 12:39:57 -0500

    Imported Assets

--- Tower Defense Rush/Assets/Scripts/EnemyMover.cs
+++ Tower Defense Rush/Assets/Scripts/EnemyMover.cs
@@ -6,7 +6,7 @@ public class EnemyMover : MonoBehaviour
 {
 
     [SerializeField] List<WayPoint> path = new List<WayPoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speedOffset = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +18,20 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(WayPoint waypoint in path)
         {
-            this.transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            // This ensures that the enemy changes directions
+            transform.LookAt(endPosition);
+
+            while(travelPercent < 1)
+            {
+                travelPercent += Time.deltaTime * speedOffset;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+            
         }
     }
 }
